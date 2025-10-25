cd6ca58142261fc0f69eabc792c05148065342e2
Anatoly <googlsocial97@gmail.com>
2025-10-26 05:37:40 +0700

    add: поиск

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,11 @@
 window.$docsify = {
     name: "Документация",
     repo: "https://github.com/gamedev-indie-team",
-    loadSidebar: true,
-    subMaxLevel: 2,
+    loadSidebar: true, // боковая панель
+    subMaxLevel: 2, // максимальный уровень вложенности оглавления
+    search: {
+        noData: "Никаких результатов!",
+        paths: "auto",
+        placeholder: "Поиск",
+    },
 };
